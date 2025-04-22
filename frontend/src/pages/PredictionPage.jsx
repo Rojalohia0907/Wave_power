32aa486a49a1d92744d5900189236665abc4d7dc
Rojalohia0907 <166741046+Rojalohia0907@users.noreply.github.com>
2025-04-22 16:31:55 +0530

    Update PredictionPage.jsx

--- frontend/src/pages/PredictionPage.jsx
+++ frontend/src/pages/PredictionPage.jsx
@@ -43,7 +43,7 @@ function PredictionPage() {
 
     setStatus("Uploading and processing...");
     try {
-      const response = await fetch("https://wave-power-back.onrender.com", {
+      const response = await fetch("https://wave-power-back.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
