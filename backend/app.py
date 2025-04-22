ac77b9e89a3409618fec04ba0c11dd840f64fd10
Rojalohia0907 <166741046+Rojalohia0907@users.noreply.github.com>
2025-04-22 14:50:13 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -112,4 +112,5 @@ def upload_files():
     return send_file(output_filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
